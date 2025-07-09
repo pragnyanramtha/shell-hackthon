4ef721193126f84cd9007688559be1deb9d3096a
pragnyanramtha <pragnyanramtha@gmail.com>
2025-07-09 09:13:38 +0000

    v5

--- basics.py
+++ basics.py
@@ -1,17 +1,46 @@
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn import linear_model
-import numpy as np
-from sklearn.datasets import load_diabetes
-
-def main():
-    X,y = load_diabetes(return_X_y=True)
-    print(X[0:5])
-    mod = KNeighborsRegressor()
-    mod.fit(X, y)
-    print(mod.predict(X[0:5]))
-    print( y[0:5])
-    # reg = linear_model.LinearRegression()
-    #reg.fit([[0, 0], [1, 1], [2, 2]], [0, 1, 2])
+import pandas as pd
+import numpy as np  
+import matplotlib.pyplot as plt
+import sklearn.model_selection as ms
+import tensorflow as tf
 
 
-main()
+df = pd.read_csv('dataset/train.csv')
+Y = df.filter(like="Blend")
+X = df.drop(columns=Y.columns, axis=1)
+
+X_train, X_val, Y_train, Y_val = ms.train_test_split(X, Y, test_size=0.2, random_state=42)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(shape=X_train.shape[1]),
+    tf.keras.layers.Dense(128,activation='relu'),
+    tf.keras.layers.Dense(Y_train.shape[1])
+])
+
+
+model.compile(optimizer='adam',loss='mse',metrics=['mae'])
+
+model.fit(X_train,Y_train,epochs=50,batch_size=32,validation_data=(X_val,Y_val))
+
+model_predictions = model.predict(X_val)
+
+
+
+
+
+
+
+
+
+
+# calculate the mean absolute percentage error (MAPE)
+mape = np.mean(np.abs((Y_val - model_predictions) / Y_val)) * 100
+print("Mean Absolute Percentage Error (MAPE):", mape)
+
+ref1 = 2.72
+ref = 2.73
+def leader_board_score(mape, x):
+    return max(10,(100 -((90 *mape )/x)))
+
+print("Leader Board Score:", leader_board_score(mape, ref1))        
+print("Leader Board Score:", leader_board_score(mape, ref))
